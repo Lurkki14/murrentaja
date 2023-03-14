48a73a2ab662b5d25fdabb9eb17e3afaad8555c2
Jussi Kuokkanen <jussi.kuokkanen@protonmail.com>
2023-03-14 11:32:24 +0200

    add optparse-applicative

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 { pkgs ? import <nixpkgs> {} }: with pkgs;
 let
-  ghc = haskellPackages.ghcWithPackages(p: [ p.text ]);
+  ghc = haskellPackages.ghcWithPackages(p: with p; [ text optparse-applicative ]);
 in
   mkShell {
     nativeBuildInputs = [ghc];
