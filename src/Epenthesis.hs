8deccf3e6ee6daf0517fda897e38d7a3c758bd42
Jussi Kuokkanen <jussi.kuokkanen@protonmail.com>
2023-03-15 12:11:00 +0200

    modify epenthesis implementation to take a list of triggers

--- src/Epenthesis.hs
+++ src/Epenthesis.hs
@@ -16,32 +16,35 @@ mkVowel x
   | elem x vowels = Just $ Vowel x
   | otherwise = Nothing
 
-mkEpenthesisTrigger :: Text -> Maybe EpenthesisTrigger
-mkEpenthesisTrigger x
-  | elem x epenthesisTriggers = Just $ EpenthesisTrigger x
+mkEpenthesisTrigger :: [Text] -> Text -> Maybe EpenthesisTrigger
+mkEpenthesisTrigger triggers x
+  | elem x triggers = Just $ EpenthesisTrigger x
   | otherwise = Nothing
 
 vowels = [ 'a', 'e', 'i', 'u', 'o', 'y', 'ä', 'ö' ] :: [Char]
 
 epenthesisTriggers = [ "lp", "lk", "lm", "lv", "nh" ] :: [Text]
 
-applyEpenthesis :: Text -> Maybe Text
-applyEpenthesis word
+applyEpenthesis = epenthesisWithTriggers epenthesisTriggers
+
+epenthesisWithTriggers :: [Text] -> Text -> Maybe Text
+epenthesisWithTriggers triggers word
   | epenthesized == word = Nothing
   | otherwise = Just epenthesized where
-  epenthesized = go "" word
-  go :: Text -> Text -> Text
-  go acc "" = acc
-  go acc text =
+  epenthesized = go triggers "" word
+  go :: [Text] -> Text -> Text -> Text
+  go _ acc "" = acc
+  go triggers acc text =
     -- 'text' in go "j" "alka" needs to be cut by 3
     -- go "jalak" "a" by 1
-    go (append acc $ fst $ nextAcc text) (T.drop (snd $ nextAcc text) text)
-  nextAcc :: Text -> (Text, Int)
-  nextAcc text =
+    let nextAcc' = nextAcc triggers text
+    in go triggers (append acc $ fst nextAcc') (T.drop (snd nextAcc') text)
+  nextAcc :: [Text] -> Text -> (Text, Int)
+  nextAcc triggers text =
     fromMaybe
       ((,) (T.take 1 text) 1)
       (uncons text >>= \(char, tail) ->
-        doEpenthesis (mkVowel char) (mkEpenthesisTrigger (T.take 2 tail)) >>= \ep ->
+        doEpenthesis (mkVowel char) (mkEpenthesisTrigger triggers (T.take 2 tail)) >>= \ep ->
           pure (ep, 3)) -- 3, eg. (length trigger) + 1
   doEpenthesis (Just (Vowel v)) (Just (EpenthesisTrigger trigger)) =
     Just $ singleton v `append` T.take 1 trigger `append`
