26cfe4ba7f5efbb6a762d9ad08e340e7b9b19587
Jussi Kuokkanen <jussi.kuokkanen@protonmail.com>
2023-03-25 14:59:08 +0200

    add diphthong reduction implementation

---
+++ src/Diphthongs.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Diphthongs where
+
+import Data.Maybe
+import Data.Text hiding (elem, drop)
+import qualified Data.Text as T
+
+import Utils
+
+-- Cause eg. poika -> poeka
+reducibleDiphthongs = [ "oi", "öi", "ai", "äi" ] :: [Text]
+
+elemFromList list x
+  | elem x list = Just x
+  | otherwise = Nothing
+
+diphthongReductionAcc :: Text -> TextAcc
+diphthongReductionAcc text =
+  maybe
+    (TextAcc 1 $ T.take 1 text)
+    (TextAcc 2 . doReduction)
+    (elemFromList reducibleDiphthongs $ T.take 2 text) where
+  doReduction text = T.take 1 text <> "e"
+
