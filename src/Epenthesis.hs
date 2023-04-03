fc1323f5b6f321299f0046749196c2d4cf59eaef
Jussi Kuokkanen <jussi.kuokkanen@protonmail.com>
2023-04-03 10:03:59 +0300

    use modifyAccumulating in epenthesis implementation

--- src/Epenthesis.hs
+++ src/Epenthesis.hs
@@ -8,6 +8,8 @@ import Data.Maybe
 import Data.Text hiding (elem, length, take)
 import qualified Data.Text as T
 
+import Utils
+
 newtype Vowel = Vowel Char
 newtype EpenthesisTrigger = EpenthesisTrigger Text
 
@@ -26,29 +28,18 @@ vowels = [ 'a', 'e', 'i', 'u', 'o', 'y', 'ä', 'ö' ] :: [Char]
 epenthesisTriggers = [ "lp", "lk", "lm", "lv", "nh" ] :: [Text]
 pohjanmaaEpenthesisTriggers = epenthesisTriggers ++ [ "hr", "hm", "hl", "hj", "hn", "hv" ]
 
-applyEpenthesis = epenthesisWithTriggers epenthesisTriggers
-applyPohjanmaaEpenthesis = epenthesisWithTriggers pohjanmaaEpenthesisTriggers
+applyEpenthesis = modifyAccumulating $ epenthesisAccWithTriggers epenthesisTriggers
+applyPohjanmaaEpenthesis =
+  modifyAccumulating $ epenthesisAccWithTriggers pohjanmaaEpenthesisTriggers
 
-epenthesisWithTriggers :: [Text] -> Text -> Maybe Text
-epenthesisWithTriggers triggers word
-  | epenthesized == word = Nothing
-  | otherwise = Just epenthesized where
-  epenthesized = go triggers "" word
-  go :: [Text] -> Text -> Text -> Text
-  go _ acc "" = acc
-  go triggers acc text =
-    -- 'text' in go "j" "alka" needs to be cut by 3
-    -- go "jalak" "a" by 1
-    let nextAcc' = nextAcc triggers text
-    in go triggers (append acc $ fst nextAcc') (T.drop (snd nextAcc') text)
-  nextAcc :: [Text] -> Text -> (Text, Int)
-  nextAcc triggers text =
-    fromMaybe
-      ((,) (T.take 1 text) 1)
-      (uncons text >>= \(char, tail) ->
-        doEpenthesis (mkVowel char) (mkEpenthesisTrigger triggers (T.take 2 tail)) >>= \ep ->
-          pure (ep, 3)) -- 3, eg. (length trigger) + 1
-  doEpenthesis (Just (Vowel v)) (Just (EpenthesisTrigger trigger)) =
-    Just $ singleton v `append` T.take 1 trigger `append`
-      singleton v `append` T.drop 1 trigger
-  doEpenthesis _ _ = Nothing
+epenthesisAccWithTriggers :: [Text] -> Text -> TextAcc
+epenthesisAccWithTriggers triggers text =
+  fromMaybe
+    (TextAcc 1 $ T.take 1 text)
+    (uncons text >>= \(char, tail) ->
+      doEpenthesis (mkVowel char) (mkEpenthesisTrigger triggers (T.take 2 tail)) >>= \ep ->
+        pure $ TextAcc 3 ep) where -- 3, eg. (length trigger) + 1
+    doEpenthesis (Just (Vowel v)) (Just (EpenthesisTrigger trigger)) =
+      Just $ singleton v `append` T.take 1 trigger `append`
+        singleton v `append` T.drop 1 trigger
+    doEpenthesis _ _ = Nothing
