145889d88ad8ac0f17f9d22faa95c8840419dc4c
Jussi Kuokkanen <jussi.kuokkanen@protonmail.com>
2023-04-04 18:25:19 +0300

    explain the purpose of modifyAccumulating in more detail

--- src/Utils.hs
+++ src/Utils.hs
@@ -10,7 +10,14 @@ data TextAcc = TextAcc {
   modified :: Text
 } deriving (Show)
 
--- Captures the repeated pattern from other modifying functions
+-- Captures the repeated pattern from all earlier modifying functions
+-- This function allows modifying words at the precision required for
+-- different transformations, and allows for the modified text to be of
+-- different length by allowing functions to specify how the modified
+-- text maps to the original word
+
+-- TODO: this function should split compound words into their elements
+-- while processing for correctness
 modifyAccumulating :: (Text -> TextAcc) -> Text -> Maybe Text
 modifyAccumulating f word
   | modified == word = Nothing
