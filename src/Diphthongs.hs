1faae3a0516c21ea18791846f0c357d88b264ff1
Jussi Kuokkanen <jussi.kuokkanen@protonmail.com>
2023-04-10 10:04:10 +0300

    implement western diphthong reduction

--- src/Diphthongs.hs
+++ src/Diphthongs.hs
@@ -12,12 +12,15 @@ import Utils
 
 -- Cause eg. poika -> poeka
 reducibleDiphthongs = [ "oi", "öi", "ai", "äi" ] :: [Text]
+-- työmies -> tyämiäs
+westernReducibleDiphthongs = [ "ie", "uo", "yö" ] :: [Text]
 
 elemFromList list x
   | elem x list = Just x
   | otherwise = Nothing
 
 applySavoReduction = modifyAccumulating diphthongReductionAcc
+applyWesternReduction = modifyAccumulating westernDiphthongReductionAcc
 
 diphthongReductionAcc :: Text -> TextAcc
 diphthongReductionAcc text =
@@ -26,3 +29,15 @@ diphthongReductionAcc text =
     (TextAcc 2 . doReduction)
     (elemFromList reducibleDiphthongs $ T.take 2 text) where
   doReduction text = T.take 1 text <> "e"
+
+westernDiphthongReductionAcc :: Text -> TextAcc
+westernDiphthongReductionAcc text =
+  maybe
+    (TextAcc 1 $ T.take 1 text)
+    (TextAcc 2 . doReduction)
+    (elemFromList westernReducibleDiphthongs $ T.take 2 text) where
+  doReduction :: Text -> Text
+  doReduction "ie" = "iä"
+  doReduction "uo" = "ua"
+  doReduction "yö" = "yä"
+  doReduction _ = ""
