707ada7e3888fcc942dde113c2743f3e39e2e6a0
Jussi Kuokkanen <jussi.kuokkanen@protonmail.com>
2023-04-10 18:31:49 +0300

    check vowel harmony for western diphthong reduction

--- src/Diphthongs.hs
+++ src/Diphthongs.hs
@@ -2,18 +2,32 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Diphthongs
-  (applySavoReduction) where
+  (applySavoReduction, applyWesternReduction) where
 
+import Data.Char
 import Data.Maybe
-import Data.Text hiding (elem, drop)
+import Data.Text hiding (any, elem, drop)
 import qualified Data.Text as T
 
 import Utils
+
+data VowelHarmony =
+  BackHarmony |
+  FrontHarmony deriving (Show)
 
 -- Cause eg. poika -> poeka
 reducibleDiphthongs = [ "oi", "öi", "ai", "äi" ] :: [Text]
 -- työmies -> tyämiäs
 westernReducibleDiphthongs = [ "ie", "uo", "yö" ] :: [Text]
+
+backVowels = [ 'a', 'o', 'u' ] :: [Char]
+
+-- Check if word contains any back vowel
+vowelHarmony :: Text -> VowelHarmony
+vowelHarmony word =
+  case any (== True) $ fmap (\c -> T.elem c word) backVowels of
+    True -> BackHarmony
+    _ -> FrontHarmony
 
 elemFromList list x
   | elem x list = Just x
@@ -34,10 +48,11 @@ westernDiphthongReductionAcc :: Text -> TextAcc
 westernDiphthongReductionAcc text =
   maybe
     (TextAcc 1 $ T.take 1 text)
-    (TextAcc 2 . doReduction)
+    (TextAcc 2 . doReduction harmony)
     (elemFromList westernReducibleDiphthongs $ T.take 2 text) where
-  doReduction :: Text -> Text
-  doReduction "ie" = "iä"
-  doReduction "uo" = "ua"
-  doReduction "yö" = "yä"
-  doReduction _ = ""
+  harmony = vowelHarmony text
+  doReduction :: VowelHarmony -> Text -> Text
+  doReduction FrontHarmony "ie" = "iä"
+  doReduction _ "uo" = "ua"
+  doReduction _ "yö" = "yä"
+  doReduction _ x = x
